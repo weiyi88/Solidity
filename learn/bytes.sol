7a6d34d11d125ead6e1c0284063550606c3909d6
Aring <tantubuping@163.com>
2023-03-02 01:26:33 +0800

    bytes add

--- learn/bytes.sol
+++ learn/bytes.sol
@@ -1,5 +1,11 @@
 // 固定长度字节数
 
+/**
+ * bytes1 一个字节，长度1
+ * bytes2 两个字节，长度2
+ * bytes12 12字节，长度12
+ * 不能被修改
+ */
 pragma solidity ^0.4.0;
 
 contract ByteArray{
@@ -7,7 +13,7 @@ contract ByteArray{
     bytes1  public num1 = 0x7a;  // 0111 1010
     bytes2  public num2 = 0x7a68;    // 0111 1010    0110    1000
 
-    bytes12 public num3 = 0x7a68656e67a69616e78756e;
+    bytes12 public num3 = 0x7a68656e67a69616e78756e;  
     
     function getLength() returns(uint){
         return num1.length;
